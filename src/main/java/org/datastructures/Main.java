e630e119a26263c617e956b5d297e4128ce6b3e6
Aaron Morris <aaron.r.morris3@gmail.com>
2023-03-06 09:16:00 -0600

    Big O Squared

--- src/main/java/org/datastructures/Main.java
+++ src/main/java/org/datastructures/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void printItems(int n) {
         for(int i = 0; i < n; i++) {
-            System.out.println(i);
-        }
+            for(int j = 0; j < n; j++){
+                System.out.println(i + " " + j);
+            }
 
-        for(int j = 0; j < n; j++){
-            System.out.println(j);
+
         }
     }
 
