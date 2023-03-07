a7b3e4a90310b93eedb7311204925e41a83cd40a
Aaron Morris <aaron.r.morris3@gmail.com>
2023-03-07 08:30:00 -0600

    Drop NonDominants

--- src/main/java/org/datastructures/Main.java
+++ src/main/java/org/datastructures/Main.java
@@ -4,13 +4,17 @@ public class Main {
 
     public static void printItems(int n) {
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++){
-                System.out.println(i + " " + j);
+            for(int j = 0; j < n; j++) {
+                    System.out.println(i + " " + j);
             }
 
-
+        }
+        for(int k = 0; k < n; k++) {
+            System.out.println(k);
         }
     }
+
+
 
     public static void main(String[] args) {
 
