953bd2e047b3924c0c64e8a8a41ab36a7649b7d3
Aaron Morris <aaron.r.morris3@gmail.com>
2023-03-09 10:47:00 -0600

    Classes

---
+++ src/main/java/org/datastructures/Cookie.java
@@ -0,0 +1,11 @@
+package org.datastructures;
+
+public class Cookie {
+    private String color;
+
+    public Cookie(String color) {
+        this.color = color;
+    }
+}
+
+Cookie cookieOne = new Cookie("green");
