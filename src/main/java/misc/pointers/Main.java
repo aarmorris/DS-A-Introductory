0d31b6ae589bf2ae4ea295bede1be4c8afb2939a
Aaron Morris <aaron.r.morris3@gmail.com>
2023-03-15 10:15:00 -0500

    Pointers

--- src/main/java/misc/pointers/Main.java
+++ src/main/java/misc/pointers/Main.java
@@ -1,12 +1,25 @@
 package misc.pointers;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
 
-        int num1 = 11;
-        int num2 = num1;
+        HashMap<String, Integer> map1 = new HashMap<>();
+        HashMap<String, Integer> map2 = new HashMap<>();
 
-        System.out.println("num1: " + num1);
-        System.out.println("num2: " + num2 );
+        map1.put("value", 11);
+        map2 = map1;
+
+        map1.put("value", 22);
+
+//        int num1 = 11;
+//        int num2 = num1;
+
+        System.out.println(map1);
+        System.out.println(map2);
+
+//        System.out.println("num1: " + num1);
+//        System.out.println("num2: " + num2 );
     }
 }
